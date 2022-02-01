d7a99b018ce6928691ed30a49c1a229fa2567fd8
andrewhills <andrewhills@users.noreply.github.com>
2022-02-01 20:03:32 +1100

    Add files via upload

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ So you can close and reopen the browser and continue youtube videos where you le
 
 ## Issue
 
-Each time the address bar is updated, an entry in the browser's history is recorded. So to reduce spamming the browser history, I made it only update the address bar every 10 seconds.
+Each time the address bar is updated, an entry in the browser's history is recorded. So to reduce spamming the browser history, I made it only update the address bar every 5 seconds.
--- myscript.js
+++ myscript.js
@@ -10,25 +10,22 @@
 
         checkExist = setInterval(() => {
             var v = document.querySelector("video");
-            var p = v.parentNode.parentNode;
+            var p = v && v.parentNode.parentNode;
             
-            if(p.getPlayerState()==1) {
+            if(p && p.getPlayerState() == 1) {
                 var pl_id_from_url = location.href.match('[&?]list=([^&]+)');
                 var pl_ind_from_url = location.href.match('[&?]index=([^&]+)');
-                
-                var vid=p.getVideoData().video_id;
-                var t=p.getCurrentTime();
                 
                 var pl_id = pl_id_from_url?pl_id_from_url[1]:null; //p.getPlaylistId();
                 var pl_ind = pl_ind_from_url?pl_ind_from_url[1]:-1; //p.getPlaylistIndex();
                 
-                var url = 'watch?v='+vid;                
-                url+= pl_id?('&list='+pl_id+'&index='+pl_ind):'';                
-                url+='&t='+t.toFixed()+'s';
+                var url = 'watch?v=' + p.getVideoData().video_id;                
+                url += pl_id?('&list=' + pl_id + '&index=' + pl_ind):'';                
+                url += '&t=' + p.getCurrentTime().toFixed() + 's';
                 
                 history.replaceState(null, '', url);
             }
-        }, 10000);
+        }, 5000);
     }
 
     document.addEventListener('yt-navigate-finish', go);
