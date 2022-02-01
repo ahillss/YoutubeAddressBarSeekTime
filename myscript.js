f83c38205d94af89641d3dd4c88cf4f53a252325
andrewhills <andrewhills@users.noreply.github.com>
2022-02-01 17:38:57 +1100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -13,12 +13,12 @@
             var p = v.parentNode.parentNode;
             
             if(p.getPlayerState()==1) {
-                var old_url = window.location.href;
-                var pl_id_from_url = old_url.match('[&?]list=([^&]+)');
-                var pl_ind_from_url = old_url.match('[&?]index=([^&]+)');
+                var pl_id_from_url = location.href.match('[&?]list=([^&]+)');
+                var pl_ind_from_url = location.href.match('[&?]index=([^&]+)');
                 
                 var vid=p.getVideoData().video_id;
                 var t=p.getCurrentTime();
+                
                 var pl_id = pl_id_from_url?pl_id_from_url[1]:null; //p.getPlaylistId();
                 var pl_ind = pl_ind_from_url?pl_ind_from_url[1]:-1; //p.getPlaylistIndex();
                 
