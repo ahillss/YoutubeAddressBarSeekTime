4f433117f0083b3745a8100efcf04347f1a46c86
andrewhills <andrewhills@users.noreply.github.com>
2022-02-13 12:57:17 +1100

    Add files via upload

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Chrome extension that modifies the address bar of youtube videos with the curren
 
 So you can close and reopen the browser and continue youtube videos where you left off.
 
+## Updates
+
+* fixed problem where the playlist index is wrong due to youtube auto incrementing it when the page is loaded
+
 ## Issue
 
 Each time the address bar is updated, an entry in the browser's history is recorded. So to reduce spamming the browser history, I made it only update the address bar every 5 seconds.
--- myscript.js
+++ myscript.js
@@ -7,21 +7,19 @@
         if(!location.pathname.startsWith('/watch')) {
             return;
         }
-
+        
         checkExist = setInterval(() => {
             var v = document.querySelector("video");
             var p = v && v.parentNode.parentNode;
             
             if(p && p.getPlayerState() == 1) {
-                var pl_id_from_url = location.href.match('[&?]list=([^&]+)');
-                var pl_ind_from_url = location.href.match('[&?]index=([^&]+)');
-                
-                var pl_id = pl_id_from_url?pl_id_from_url[1]:null; //p.getPlaylistId();
-                var pl_ind = pl_ind_from_url?pl_ind_from_url[1]:-1; //p.getPlaylistIndex();
-                
-                var url = 'watch?v=' + p.getVideoData().video_id;                
-                url += pl_id?('&list=' + pl_id + '&index=' + pl_ind):'';                
-                url += '&t=' + p.getCurrentTime().toFixed() + 's';
+                var m = document.evaluate(
+                    '//ytd-playlist-panel-renderer[@id="playlist" and contains(@class,"ytd-watch-flexy")]//span[@id="index" and .="\u25b6"]/parent::*/parent::*//a/@href',
+                    document,null,XPathResult.STRING_TYPE,null).stringValue.match('list=([^&]+)&index=([^&]+)');
+
+                var url = 'watch?v=' + p.getVideoData().video_id;
+                url+= m?('&list=' + m[1] + '&index=' + m[2]):'';
+                url+= '&t=' + p.getCurrentTime().toFixed() + 's';
                 
                 history.replaceState(null, '', url);
             }
