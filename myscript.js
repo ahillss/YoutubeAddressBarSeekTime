c52502c99445328592581264ff0e8ca0a0bc3bd0
ahillss <5051345+ahillss@users.noreply.github.com>
2024-06-12 00:47:21 +1000

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,6 +1,24 @@
 (()=>{
     var checkExist = null;
     
+    function update_url() {
+        var m = document.evaluate(
+            '//ytd-playlist-panel-renderer[@id="playlist" and contains(@class,"ytd-watch-flexy")]//span[@id="index" and .="\u25b6"]/parent::*/parent::*//a/@href',
+            document,null,XPathResult.STRING_TYPE,null).stringValue.match('list=([^&]+)&index=([^&]+)');
+
+        var url = 'watch?v=' + movie_player.getVideoData().video_id;
+        url+= m?('&list=' + m[1] + '&index=' + m[2]):'';
+        url+= '&t=' + movie_player.getCurrentTime().toFixed() + 's';
+        
+        history.replaceState(null, '', url);
+    }
+    
+    //~ function playerEvent(event) {
+        //~ if (event.data==2) { //pause
+            //~ update_url();
+        //~ }
+    //~ }
+    
     function go() {
         clearInterval(checkExist);
         
@@ -8,22 +26,16 @@
             return;
         }
         
+        //~ setTimeout(function(){
+        
+            //~ movie_player.addEventListener("onStateChange", playerEvent);
+        //~ }, 10000);
+        
         checkExist = setInterval(() => {
-            var v = document.querySelector("video");
-            var p = v && v.parentNode.parentNode;
-            
-            if(p && p.getPlayerState() == 1) {
-                var m = document.evaluate(
-                    '//ytd-playlist-panel-renderer[@id="playlist" and contains(@class,"ytd-watch-flexy")]//span[@id="index" and .="\u25b6"]/parent::*/parent::*//a/@href',
-                    document,null,XPathResult.STRING_TYPE,null).stringValue.match('list=([^&]+)&index=([^&]+)');
-
-                var url = 'watch?v=' + p.getVideoData().video_id;
-                url+= m?('&list=' + m[1] + '&index=' + m[2]):'';
-                url+= '&t=' + p.getCurrentTime().toFixed() + 's';
-                
-                history.replaceState(null, '', url);
+            if(typeof movie_player !== 'undefined' && movie_player && movie_player.getPlayerState() == 1) {
+                update_url();
             }
-        }, 5000);
+        }, 10000);
     }
 
     document.addEventListener('yt-navigate-finish', go);
